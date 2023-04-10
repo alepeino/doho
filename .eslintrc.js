73fd9bb7f66e616c46eed2e981be51ca685a56bf
Alejandro Peinó <alepeino@gmail.com>
2023-04-09 23:56:29 -0300

    refactor: move store to src file

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
--- test/basic.test.tsx
+++ test/basic.test.tsx
@@ -1,64 +1,7 @@
 import { fireEvent, render } from "@testing-library/react";
-import { createElement, useCallback, useEffect, useState } from "react";
+import { createElement, useState } from "react";
 import { expectType } from "ts-expect";
-
-type MethodsOf<T> = {
-  [M in keyof T as T[M] extends (...args: unknown[]) => unknown
-    ? M
-    : never]: T[M] extends (...args: infer P) => infer R
-    ? (...args: P) => R
-    : never;
-};
-
-type Listener = () => void;
-
-class Store<T> {
-  private listeners: Listener[] = [];
-
-  constructor(private value: T) {}
-
-  useRead<R>(selector: (value: T) => R) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const getter = useCallback(
-      () => ({ value: selector(this.value) }),
-      [selector]
-    );
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [state, setState] = useState(getter);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => this.addListener(() => setState(getter)), [getter]);
-
-    return state.value;
-  }
-
-  useWrite<R extends MethodsOf<T>[keyof MethodsOf<T>]>(
-    selector: (value: MethodsOf<T>) => R
-  ): R {
-    const selectedMethod = selector(this.value as MethodsOf<T>);
-    return ((...args) => {
-      const result = selectedMethod.call(this.value, ...args);
-      this.emitChange();
-      return result;
-    }) as typeof selectedMethod;
-  }
-
-  private emitChange() {
-    for (const listener of this.listeners) {
-      listener();
-    }
-  }
-
-  private addListener(listener) {
-    this.listeners.push(listener);
-    return () => {
-      this.removeListener(listener);
-    };
-  }
-
-  private removeListener(listener) {
-    this.listeners = this.listeners.filter((l) => l !== listener);
-  }
-}
+import { Store } from "../src/store";
 
 describe("...", () => {
   it("reads a primitive value", async () => {
